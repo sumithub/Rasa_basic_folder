b0d76e00eabc98e592e617d328977e59764587bd
Sumit Arora <sumit101@gmail.com>
2021-04-14 16:21:28 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rasa_basic_folder
+ Restaurant finder Rasa based Chat bot
---
+++ actions.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+import pandas as pd
+import json
+
+ZomatoData = pd.read_csv('zomato.csv')
+ZomatoData = ZomatoData.drop_duplicates().reset_index(drop=True)
+WeOperate = ['New Delhi', 'Gurgaon', 'Noida', 'Faridabad', 'Allahabad', 'Bhubaneshwar', 'Mangalore', 'Mumbai', 'Ranchi', 'Patna', 'Mysore', 'Aurangabad', 'Amritsar', 'Puducherry', 'Varanasi', 'Nagpur', 'Vadodara', 'Dehradun', 'Vizag', 'Agra', 'Ludhiana', 'Kanpur', 'Lucknow', 'Surat', 'Kochi', 'Indore', 'Ahmedabad', 'Coimbatore', 'Chennai', 'Guwahati', 'Jaipur', 'Hyderabad', 'Bangalore', 'Nashik', 'Pune', 'Kolkata', 'Bhopal', 'Goa', 'Chandigarh', 'Ghaziabad', 'Ooty', 'Gangtok', 'Shimla']
+
+def RestaurantSearch(City,Cuisine):
+	TEMP = ZomatoData[(ZomatoData['Cuisines'].apply(lambda x: Cuisine.lower() in x.lower())) & (ZomatoData['City'].apply(lambda x: City.lower() in x.lower()))]
+	return TEMP[['Restaurant Name','Address','Average Cost for two','Aggregate rating']]
+
+class ActionSearchRestaurants(Action):
+	def name(self):
+		return 'action_search_restaurants'
+
+	def run(self, dispatcher, tracker, domain):
+		#config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
+		loc = tracker.get_slot('location')
+		cuisine = tracker.get_slot('cuisine')
+		results = RestaurantSearch(City=loc,Cuisine=cuisine)
+		response=""
+		if results.shape[0] == 0:
+			response= "no results"
+		else:
+			for restaurant in RestaurantSearch(loc,cuisine).iloc[:5].iterrows():
+				restaurant = restaurant[1]
+				response=response + F"Found {restaurant['Restaurant Name']} in {restaurant['Address']} rated {restaurant['Address']} with avg cost {restaurant['Average Cost for two']} \n\n"
+				
+		dispatcher.utter_message("-----"+response)
+		return [SlotSet('location',loc)]
+
+class ActionSendMail(Action):
+	def name(self):
+		return 'action_send_mail'
+
+	def run(self, dispatcher, tracker, domain):
+		MailID = tracker.get_slot('mail_id')
+		sendmail(MailID,response)
+		return [SlotSet('mail_id',MailID)]
